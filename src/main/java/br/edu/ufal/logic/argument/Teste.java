f9226eb7e5255657b1bfd1bcde221714fdb730e0
Douglas Alves <felipe.douglas.alves@hotmail.com>
2022-11-15 22:32:03 -0300

    Teste de Leitor txt

---
+++ src/main/java/br/edu/ufal/logic/argument/Teste.java
@@ -0,0 +1,25 @@
+package br.edu.ufal.logic.argument;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Teste {
+
+   
+
+    public void lerTexto() throws IOException{
+        
+    }
+    public static void main(String[] args) throws IOException {
+        Path caminho = Paths.get("C:/Users/Douglas/Desktop/Alloy/AlloyTexto.txt");
+        byte[] texto = Files.readAllBytes(caminho);
+        String textoAlloy = new String(texto);
+
+
+        System.out.println(textoAlloy);
+    }
+
+    
+}
